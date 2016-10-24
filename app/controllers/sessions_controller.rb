e2beb494926842d5e4e4c38122be381610cee672
Niti Nrw <niti.patel@flatironschool.com>
2016-10-24 14:33:15 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name] != "" && params[:name] != nil
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to '/sessions/new'
+    end
+  end
+
+  def destroy
+  session.delete :name
+  session[:name] = nil
+  redirect_to '/'
+  end
+end
